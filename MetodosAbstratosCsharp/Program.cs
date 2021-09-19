96cd38ff52520aa60a928eaecdcd486160538c92
Ingrith Reis <ingrith.reis@hotmail.com>
2021-09-18 21:07:28 -0300

    Adicionado CultureInfo.InvariantCulture no valor soma final e pequenos testes de edição.

--- MetodosAbstratosCsharp/Program.cs
+++ MetodosAbstratosCsharp/Program.cs
@@ -16,40 +16,49 @@ namespace MetodosAbstratosCsharp
             for(int i = 1; i<=n; i++)
             {
                 Console.WriteLine($"Entre com os dados do #{i} contrinuinte");
-                Console.Write("Pessoa Física ou Jurídia? F/J");
+                Console.Write("Pessoa Física ou Jurídia? F/J ");
                 string resposta = Console.ReadLine();
 
-                Console.Write("Nome: ");
-                string nome = Console.ReadLine();
-                Console.Write("Renda anual declarada: ");
-                double renda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
+                
+                
                 if (resposta == "F" || resposta == "f")
                 {
+                    Console.Write("Nome: ");
+                    string nome = Console.ReadLine();
+                    Console.Write("Renda anual declarada: ");
+                    double renda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                     Console.Write("Valor com gastos em saúde: ");
                     double gastos = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
                     pessoas.Add(new PessoaFisica(nome, renda, gastos));
 
                 }
-                else
+                else if(resposta == "J" || resposta == "j")
                 {
+                    Console.Write("Nome: ");
+                    string nome = Console.ReadLine();
+                    Console.Write("Renda anual declarada: ");
+                    double renda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                     Console.Write("Quantidade de funcionários: ");
                     int funcionarios = int.Parse(Console.ReadLine());
 
                     pessoas.Add(new PessoaJuridica(nome, renda, funcionarios));
                 }
+                else 
+                {
+                    Console.WriteLine("Dado inserido incorretamente, favor refazer solicitação deste contribuinte");
+                }
             }
-
-            Console.WriteLine("Contrinuintes ");
+            Console.WriteLine(" ");
+            Console.WriteLine("Contrinuintes: ");
             double soma = 0;
             foreach (Pessoa pessoa in pessoas)
             { 
-                Console.WriteLine(pessoa.ValorPago().ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine(pessoa.Nome +" $"+ pessoa.ValorPago().ToString("F2", CultureInfo.InvariantCulture));
                 soma += pessoa.ValorPago();
             }
 
-            Console.WriteLine($"Total de valor arrecadado: {soma}");
+            Console.WriteLine("Total de valor arrecadado: "+ soma.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
